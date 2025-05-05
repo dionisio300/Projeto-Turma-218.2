912fcb438723de34543b786b06428f67801d0bbb
dionisio300 <dionisioassis80@gmail.com>
2025-05-05 20:28:33 -0300

    Criação dos arquivos de trabalho do Gabriel e da Alexya

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print('Esse é o arquivo principal')
+
+'''
+1 - O líder do projeto cria o repositório e convida os outros para colaborarem
+2 - Os colaboradores aceitam o convite e podem clonar o repositório
+'''
