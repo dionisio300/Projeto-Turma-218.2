3dc9c2953cc0ee279c529e54923c96cf557dcf63
dionisio300 <dionisioassis80@gmail.com>
2025-05-05 20:58:11 -0300

    Novas alterações

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ print('Esse é o arquivo principal')
 1 - O líder do projeto cria o repositório e convida os outros para colaborarem
 2 - Os colaboradores aceitam o convite e podem clonar o repositório
 '''
+
+#Construir a função de conexão com o banco de dados - Alexya
+
+
+
+#Construir a função para cadastrar um novo cliente no banco - Gabriel
+
+
+
+# Exibir todos os clientes que estão no banco de dados - Dionizio
+
+
+
+
